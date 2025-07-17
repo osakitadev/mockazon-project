0b1ec0eb5ecf0aee5f59e81ee5245d1ad21984c8
Osaka <138826243+osakitadev@users.noreply.github.com>
2025-07-17 14:04:44 -0600

    Added product filtering

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import './style.css'
 import products from './mock/products_mock.json'
 
 import createCategoryOptions from './categories/create-category-options'
 import listPopularCategories from './categories/list-popular'
 import setupProductLazyLoader from './products/lazy-loader'
 import addProductToCart from './shopping-cart/add-product'
+import setupProductFilter from './products/filter-products'
 
 const $cartButton = document.getElementById('cart-button')
 const $closeCartModalBtn = document.getElementById('close-cart-modal')
@@ -14,6 +14,7 @@ const $cartModal = document.getElementById('modal-cart')
   createCategoryOptions(products)
   listPopularCategories(products)
   setupProductLazyLoader(products)
+  setupProductFilter()
 
   $cartButton.addEventListener('click', () =>
     $cartModal.classList.remove('hidden')
